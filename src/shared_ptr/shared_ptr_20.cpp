8363e7b56bb5d3cc0b87f792af7ea631814a8c63
kudakov <kudakov@inspector-cloud.com>
2024-10-30 01:09:47 +0300

    add experemets with atomic shared_ptr

--- src/shared_ptr/shared_ptr_20.cpp
+++ src/shared_ptr/shared_ptr_20.cpp
@@ -4,6 +4,8 @@
 #include <string>
 #include <thread>
 
+
+// THIS IS NOT THREAD-SAFE CODE.
 int main(int argc, char *argv[])
 {
     std::cout << "\n";
---
+++ src/shared_ptr/shared_ptr_20_test1.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <memory>
+#include <atomic>
+#include <thread>
+#include <chrono>
+
+int main(int argc, char *argv[])
+{
+    std::cout << "\n";
+
+    std::atomic<std::shared_ptr<std::string>> shared_string = std::make_shared<std::string>("Zero");
+
+    // Capture the existing shared_ptr to use in lambdas
+    auto existing_string = shared_string.load();
+
+    std::thread t1([&shared_string, existing_string]()
+                   { 
+        existing_string->append("One"); 
+        shared_string.store(existing_string); });
+
+    std::thread t2([&shared_string, existing_string]()
+                   { 
+        std::this_thread::sleep_for(std::chrono::nanoseconds(1000));
+        existing_string->append("Two"); 
+        shared_string.store(existing_string); });
+
+    std::thread t3([&shared_string, existing_string]()
+                   { 
+        existing_string->append("Three"); 
+        shared_string.store(existing_string); });
+
+    std::thread t4([&shared_string, existing_string]()
+                   { 
+        existing_string->append("Four"); 
+        shared_string.store(existing_string); });
+
+    std::thread t5([&shared_string, existing_string]()
+                   { 
+        existing_string->append("Five"); 
+        shared_string.store(existing_string); });
+
+    t1.join();
+    t2.join();
+    t3.join();
+    t4.join();
+    t5.join();
+    std::cout << *shared_string.load() << std::endl;
+    return 0;
+}
