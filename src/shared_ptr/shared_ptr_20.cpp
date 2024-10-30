1daf7e7abc5095746c5a43e24f3e2d2d494b1b8d
kudakov <kudakov@inspector-cloud.com>
2024-10-31 00:44:12 +0300

    fix format@

--- src/shared_ptr/shared_ptr_20.cpp
+++ src/shared_ptr/shared_ptr_20.cpp
@@ -1,38 +1,47 @@
+#include <atomic>
 #include <iostream>
 #include <memory>
-#include <atomic>
 #include <string>
 #include <thread>
 
-
 // THIS IS NOT THREAD-SAFE CODE.
-int main(int argc, char *argv[])
-{
-    std::cout << "\n";
+int main(int argc, char *argv[]) {
+  std::cout << "\n";
 
-    std::atomic<std::shared_ptr<std::string>> shared_string = std::make_shared<std::string>("Zero");
+  std::atomic<std::shared_ptr<std::string>> shared_string =
+      std::make_shared<std::string>("Zero");
 
-    std::thread t1([&shared_string]
-                   { shared_string.store(std::make_shared<std::string>(*shared_string.load() + "One")); });
+  std::thread t1([&shared_string] {
+    shared_string.store(
+        std::make_shared<std::string>(*shared_string.load() + "One"));
+  });
 
-    std::thread t2([&shared_string]
-                   { shared_string.store(std::make_shared<std::string>(*shared_string.load() + "Two")); });
+  std::thread t2([&shared_string] {
+    shared_string.store(
+        std::make_shared<std::string>(*shared_string.load() + "Two"));
+  });
 
-    std::thread t3([&shared_string]()
-                   { shared_string.store(std::make_shared<std::string>(*shared_string.load() + "Three")); });
+  std::thread t3([&shared_string]() {
+    shared_string.store(
+        std::make_shared<std::string>(*shared_string.load() + "Three"));
+  });
 
-    std::thread t4([&shared_string]()
-                   { shared_string.store(std::make_shared<std::string>(*shared_string.load() + "Four")); });
+  std::thread t4([&shared_string]() {
+    shared_string.store(
+        std::make_shared<std::string>(*shared_string.load() + "Four"));
+  });
 
-    std::thread t5([&shared_string]()
-                   { shared_string.store(std::make_shared<std::string>(*shared_string.load() + "Five")); });
+  std::thread t5([&shared_string]() {
+    shared_string.store(
+        std::make_shared<std::string>(*shared_string.load() + "Five"));
+  });
 
-    t1.join();
-    t2.join();
-    t3.join();
-    t4.join();
-    t5.join();
+  t1.join();
+  t2.join();
+  t3.join();
+  t4.join();
+  t5.join();
 
-    std::cout << *shared_string.load() << std::endl;
-    return 0;
+  std::cout << *shared_string.load() << std::endl;
+  return 0;
 }
